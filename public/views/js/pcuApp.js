7fa60f106f53dd4fa8fe5a85f74adef1519fbe07
Riclas1 <ricosonntag@gmx.net>
2017-04-24 21:58:58 +0200

    aa

--- public/views/js/pcuApp.js
+++ public/views/js/pcuApp.js
@@ -39,31 +39,6 @@ app.config(function($routeProvider){
 });*/
 
 
-/*app.controller('mainController',['$scope', '$log', '$http', function($scope, $log, $http){
-	$scope.$log = $log;
-	$scope.value = "";
-	$scope.Pos= "";
-	$scope.Drehzahl = "";
-	$scope.ajaxPost = function(slot,value){
-		$scope.value = "senden";
-		var req = {
- 				method: 'POST',
- 				url: '/api/Umrichter/' + slot,
- 				headers: {
-   					'Content-Type': undefined
-	 			},
- 				data: value  
-		}
-		$http(req).then(function(){$scope.value = "gesendet";}, function(){$scope.value = "fehler";});
-	};
-	
-	$scope.$on('$viewContentLoaded', function(event) {
-		$scope.value = "1";
-	});
-
-	
-}]);*/
-
 
 app.controller('authController', function($scope, $http, $rootScope, $location){
 	$scope.user = {username: '', password: ''};
