f23406cb03bdd532e080a564a96adef86fcd2996
Riclas1 <ricosonntag@gmx.net>
2017-05-08 10:40:56 +0200

    bower

--- server/config/auth/isAuthenticated.routes.js
+++ server/config/auth/isAuthenticated.routes.js
@@ -1,10 +1,7 @@
 module.exports = function (req, res, next) {
 	
     // if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	console.log(req.session);
-
+		
 	if (req.isAuthenticated()){
 		console.log('auth gültig');
 		return next();
